00cfd931520a25b253632bda20102fba62779334
lazar.stojcevic <stojcevic00@gmail.com>
2022-10-30 21:01:39 +0100

    Remove address regex

--- HospitalApplication/HospitalApi/Domain/Common/Patient/Adress.cs
+++ HospitalApplication/HospitalApi/Domain/Common/Patient/Adress.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.RegularExpressions;
-using FluentValidation;
+﻿using FluentValidation;
 using FluentValidation.Results;
 using ValueOf;
 
@@ -7,12 +6,9 @@ namespace HospitalApi.Domain.Common.Patient
 {
     public class Adress : ValueOf<string, Adress>
     {
-        private static readonly Regex AdressRegex =
-            new(@"^[A-Za-z0-9]+(?:\s[A-Za-z0-9'_-]+)+$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-
         protected override void Validate()
         {
-            if (!AdressRegex.IsMatch(Value))
+            if (Value.Length < 4 || Value.Length > 256)
             {
                 var message = $"{Value} is not a valid adress";
                 throw new ValidationException(message, new[]
